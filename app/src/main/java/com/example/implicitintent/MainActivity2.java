c2c4db2b26af9243b2963e02d3ae29016893e72b
usama <bsef18@43>
2021-06-10 02:19:03 +0500

    Explicit

---
+++ app/src/main/java/com/example/implicitintent/MainActivity2.java
@@ -0,0 +1,71 @@
+package com.example.implicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    Button button;
+    TextView textView;
+    TextView inputText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button=findViewById(R.id.button2);
+        inputText=findViewById(R.id.inputBox);
+        textView=findViewById(R.id.showText);
+
+    }
+
+    public void ToDoList(View view)
+    {
+        textView.append("->  ");
+        textView.append( inputText.getText());
+        inputText.setText("");
+        textView.append("\n");
+    }
+
+
+    public void Clear(View view)
+    {
+        textView.setText("");
+        inputText.setText("");
+    }
+
+    public void Google(View view)
+    {
+        Uri uri = Uri.parse("https://www.google.com");
+        Intent intent = new Intent (Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void Call(View view)
+    {
+        Uri uri = Uri.parse("tel:+923214663901");
+        Intent intent = new Intent (Intent.ACTION_DIAL, uri);
+        startActivity(intent);
+
+    }
+
+    public void Github(View view)
+    {
+        Uri uri = Uri.parse("https://github.com/usamaimtiaz32?tab=repositories");
+        Intent intent = new Intent (Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+
+
+
+
+}
